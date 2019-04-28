ed303516b4fad180ee4e6730657abfde165078b7
ahmmedsobh <32520715+ahmmedsobh@users.noreply.github.com>
2019-04-28 22:01:35 +0200

    Add files via upload

---
+++ SimplePaint/SimplePaint/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace SimplePaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.trBrushSize = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trBrushSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 23);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // trBrushSize
+            // 
+            this.trBrushSize.Location = new System.Drawing.Point(42, 12);
+            this.trBrushSize.Maximum = 100;
+            this.trBrushSize.Minimum = 1;
+            this.trBrushSize.Name = "trBrushSize";
+            this.trBrushSize.Size = new System.Drawing.Size(104, 45);
+            this.trBrushSize.TabIndex = 1;
+            this.trBrushSize.Value = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 337);
+            this.Controls.Add(this.trBrushSize);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.trBrushSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trBrushSize;
+    }
+}
+
---
+++ SimplePaint/SimplePaint/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplePaint
+{
+    public partial class Form1 : Form
+    {
+        Brush PaintBrush = new SolidBrush(Color.FromName("Red"));
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // mouse left button paint 
+            if(e.Button == MouseButtons.Left)
+            {
+                //create paint graphic object
+                Graphics graphics = this.CreateGraphics();
+                // create ellipse fill it red color
+                graphics.FillEllipse(PaintBrush, e.X, e.Y, trBrushSize.Value, trBrushSize.Value);
+            }
+            // mouse right button eraser
+            else if (e.Button == MouseButtons.Right)
+            {
+                // create eraser brush
+                Brush b1 = new SolidBrush(this.BackColor);
+                //create eraser graphic object
+                Graphics graphics = this.CreateGraphics();
+                // create ellipse fill it red color
+                graphics.FillEllipse(b1, e.X, e.Y, 20, 20);
+            }
+           
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // get brush color from color dialog
+            ColorDialog cd = new ColorDialog();
+            if(cd.ShowDialog() == DialogResult.OK)
+            {
+                // send color to paint brush
+                PaintBrush = new SolidBrush(cd.Color);
+                // change button color to selected color
+                button1.BackColor = cd.Color;
+            }
+        }
+    }
+}
